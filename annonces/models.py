15ff33c05ef69e70b5789a84bda7f1b0d5b80c00
Geoffroy Blondel <geoffroy.blondel@gmail.com>
2021-03-13 13:34:25 +0100

    Add source to annonce model and add choice enums for source and status.

--- annonces/models.py
+++ annonces/models.py
@@ -1,12 +1,19 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
+
+class Status(models.TextChoices):
+    ATTENTE = 'ATTENTE', _('En attente')
+    VALIDE = 'VALIDE', _('Valide')
+    ERREUR = 'ERREUR', _('En erreur')
+
+
+class Sources(models.TextChoices):
+    LEBONCOIN = 'LEBONCOIN', _('Leboncoin')
+    PAP = 'PAP', _('Particuliers à particuliers')
 
 class Annonce(models.Model):
-    STATUS_CHOIX = [
-        ('ATTENTE', 'ATTENTE'),
-        ('VALIDE', 'VALIDE'),
-        ('ERREUR', 'ERREUR'),
-    ]
+    source = models.CharField(max_length=10, choices=Sources.choices, null=True)
     lien = models.URLField(max_length=200)
     titre = models.CharField(max_length=100, null=True)
     prix = models.IntegerField(null=True)
@@ -14,7 +21,7 @@ class Annonce(models.Model):
     description = models.TextField(null=True)
     code_postal = models.CharField(max_length=5, null=True)
     date_publication = models.DateField(null=True)
-    status = models.CharField(max_length=10, choices=STATUS_CHOIX, default='ATTENTE',)
+    status = models.CharField(max_length=10, choices=Status.choices, default=Status.ATTENTE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
